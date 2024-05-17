85f7554e009fd35bc3e2b4ffdd30280ad18eb494
davidearbola <davidearbola@gmail.com>
2024-05-17 12:46:27 +0200

    aggiunta ultimo messaggio colonna contatti e aggiunta orario corrente

--- js/main.js
+++ js/main.js
@@ -15,17 +15,20 @@ createApp({
 					visible: true,
 					messages: [
 						{
-							date: "10/01/2020 15:30:55",
+							date: "10/01/2020",
+							hour: "15:30",
 							message: "Hai portato a spasso il cane?",
 							status: "sent",
 						},
 						{
-							date: "10/01/2020 15:50:00",
+							date: "10/01/2020",
+							hour: "15:50",
 							message: "Ricordati di stendere i panni",
 							status: "sent",
 						},
 						{
-							date: "10/01/2020 16:15:22",
+							date: "10/01/2020",
+							hour: "16:15",
 							message: "Tutto fatto!",
 							status: "received",
 						},
@@ -37,17 +40,20 @@ createApp({
 					visible: true,
 					messages: [
 						{
-							date: "20/03/2020 16:30:00",
+							date: "20/03/2020",
+							hour: "16:30",
 							message: "Ciao come stai?",
 							status: "sent",
 						},
 						{
-							date: "20/03/2020 16:30:55",
+							date: "20/03/2020",
+							hour: "16:30",
 							message: "Bene grazie! Stasera ci vediamo?",
 							status: "received",
 						},
 						{
-							date: "20/03/2020 16:35:00",
+							date: "20/03/2020",
+							hour: "16:35",
 							message:
 								"Mi piacerebbe ma devo andare a fare la spesa.",
 							status: "sent",
@@ -60,17 +66,20 @@ createApp({
 					visible: true,
 					messages: [
 						{
-							date: "28/03/2020 10:10:40",
+							date: "28/03/2020",
+							hour: "10:10",
 							message: "La Marianna va in campagna",
 							status: "received",
 						},
 						{
-							date: "28/03/2020 10:20:10",
+							date: "28/03/2020",
+							hour: "10:20",
 							message: "Sicuro di non aver sbagliato chat?",
 							status: "sent",
 						},
 						{
-							date: "28/03/2020 16:15:22",
+							date: "28/03/2020",
+							hour: "16:15",
 							message: "Ah scusa!",
 							status: "received",
 						},
@@ -82,12 +91,14 @@ createApp({
 					visible: true,
 					messages: [
 						{
-							date: "10/01/2020 15:30:55",
+							date: "10/01/2020",
+							hour: "15:30",
 							message: "Lo sai che ha aperto una nuova pizzeria?",
 							status: "sent",
 						},
 						{
-							date: "10/01/2020 15:50:00",
+							date: "10/01/2020",
+							hour: "15:50",
 							message: "Si, ma preferirei andare al cinema",
 							status: "received",
 						},
@@ -99,12 +110,14 @@ createApp({
 					visible: true,
 					messages: [
 						{
-							date: "10/01/2020 15:30:55",
+							date: "10/01/2020",
+							hour: "15:30",
 							message: "Ricordati di chiamare la nonna",
 							status: "sent",
 						},
 						{
-							date: "10/01/2020 15:50:00",
+							date: "10/01/2020",
+							hour: "15:50",
 							message: "Va bene, stasera la sento",
 							status: "received",
 						},
@@ -116,17 +129,20 @@ createApp({
 					visible: true,
 					messages: [
 						{
-							date: "10/01/2020 15:30:55",
+							date: "10/01/2020",
+							hour: "15:30",
 							message: "Ciao Claudia, hai novità?",
 							status: "sent",
 						},
 						{
-							date: "10/01/2020 15:50:00",
+							date: "10/01/2020",
+							hour: "15:50",
 							message: "Non ancora",
 							status: "received",
 						},
 						{
-							date: "10/01/2020 15:51:00",
+							date: "10/01/2020",
+							hour: "15:51",
 							message: "Nessuna nuova, buona nuova",
 							status: "sent",
 						},
@@ -138,13 +154,15 @@ createApp({
 					visible: true,
 					messages: [
 						{
-							date: "10/01/2020 15:30:55",
+							date: "10/01/2020",
+							hour: "15:30",
 							message:
 								"Fai gli auguri a Martina che è il suo compleanno!",
 							status: "sent",
 						},
 						{
-							date: "10/01/2020 15:50:00",
+							date: "10/01/2020",
+							hour: "15:50",
 							message:
 								"Grazie per avermelo ricordato, le scrivo subito!",
 							status: "received",
@@ -157,19 +175,22 @@ createApp({
 					visible: true,
 					messages: [
 						{
-							date: "10/01/2020 15:30:55",
+							date: "10/01/2020",
+							hour: "15:30",
 							message:
 								"Ciao, andiamo a mangiare la pizza stasera?",
 							status: "received",
 						},
 						{
-							date: "10/01/2020 15:50:00",
+							date: "10/01/2020",
+							hour: "15:50",
 							message:
 								"No, l'ho già mangiata ieri, ordiniamo sushi!",
 							status: "sent",
 						},
 						{
-							date: "10/01/2020 15:51:00",
+							date: "10/01/2020",
+							hour: "15:51",
 							message: "OK!!",
 							status: "received",
 						},
@@ -186,15 +207,25 @@ createApp({
 			this.contacts[this.contactActive].messages.push({
 				message: this.inputValue,
 				status: "sent",
+				hour: this.getNow(),
 			});
 			this.inputValue = "";
 			setTimeout(() => {
 				this.contacts[this.contactActive].messages.push({
 					message: "Ok",
 					status: "received",
+					hour: this.getNow(),
 				});
 			}, 1000);
 		},
+		getNow() {
+			const today = new Date();
+			const time = today.getHours() + ":" + today.getMinutes();
+			return time;
+		},
 	},
-	mounted() {},
+	mounted() {
+		this.getNow();
+		console.log(this.getNow());
+	},
 }).mount("#app");
